be888093885f404f38d3998d6bc966f76ebce465
lengo <chendaye666@gmail.com>
2019-10-22 16:35:04 +0800

    page

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -31,72 +31,9 @@ export default new Router({
           name: 'upload'
         },
         {
-          path: 'weibo',
-          component: require('@/pages/picbeds/Weibo').default,
-          name: 'weibo'
-        },
-        {
-          path: 'qiniu',
-          component: require('@/pages/picbeds/Qiniu').default,
-          name: 'qiniu'
-        },
-        {
-          path: 'tcyun',
-          component: require('@/pages/picbeds/TcYun').default,
-          name: 'tcyun'
-        },
-        {
-          path: 'upyun',
-          component: require('@/pages/picbeds/UpYun').default,
-          name: 'upyun'
-        },
-        {
-          path: 'github',
-          component: require('@/pages/picbeds/GitHub').default,
-          name: 'github'
-        },
-        {
-          path: 'smms',
-          component: require('@/pages/picbeds/SMMS').default,
-          name: 'smms'
-        },
-        {
-          path: 'aliyun',
-          component: require('@/pages/picbeds/AliYun').default,
-          name: 'aliyun'
-        },
-        {
-          path: 'imgur',
-          component: require('@/pages/picbeds/Imgur').default,
-          name: 'imgur'
-        },
-        {
-          path: 'others/:type',
-          component: require('@/pages/picbeds/Others').default,
-          name: 'others'
-        },
-        {
-          path: 'gallery',
-          component: require('@/pages/Gallery').default,
-          name: 'gallery',
-          meta: {
-            keepAlive: true
-          }
-        },
-        {
           path: 'setting',
           component: require('@/pages/PicGoSetting').default,
           name: 'setting'
-        },
-        {
-          path: 'plugin',
-          component: require('@/pages/Plugin').default,
-          name: 'plugin'
-        },
-        {
-          path: 'shortcut-page',
-          component: require('@/pages/ShortCutPage').default,
-          name: 'shortcut-page'
         }
       ]
     },
