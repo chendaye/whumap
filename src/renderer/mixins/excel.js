e0fe9be43bc4b3e04b0ce6f394164027e50189f3
lengo <chendaye666@gmail.com>
2019-11-05 14:18:06 +0800

    fuck

--- src/renderer/mixins/excel.js
+++ src/renderer/mixins/excel.js
@@ -1,3 +1,4 @@
+
 export const excel = {
   data() {
     return {
@@ -5,6 +6,7 @@ export const excel = {
       radius: 0.3, // 300m
       excel: new Map(), // excel数据
       list: [], // 数组数据
+      obj: [], // 数组数据
       // keyword: ['商场', '超市', '快餐店', '交叉路口'], // 个数或距离，面积
       filename: '',
       autoWidth: true,
@@ -16,44 +18,47 @@ export const excel = {
     }
   },
   methods: {
+    // map 转 对象
+    strMapToObj(strMap) {
+      let obj = Object.create(null)
+      for (let [k, v] of strMap) {
+        obj[k] = v
+      }
+      return obj
+    },
+    // map 转 数组
+    strMapToArr(strMap) {
+      let arr = []
+      for (let v of strMap) {
+        arr.push(v[1].point)
+      }
+      return arr
+    },
     // 搜索结果
     result() {
-      // excel数据
-      if (this.isCircle) {
-        for (let item of this.options.entries()) {
-          this.excel.set(item[0], this.searchNearby(item))
-        }
-        // 重组excel数据
-        this.toExcel()
-        // 下载excel
-        // this.handleDownload()
-      } else {
-        this.searchInBounds()
+      // let obj = this.strMapToObj(this.options)
+      // let arr = this.strMapToArr(this.options)
+      // for循环执行时不会等待异步函数执行
+      for (let item of this.options.values()) {
+        // 异步
+        this.searchNearby(item, (res) => {
+          this.list.push(res)
+        })
       }
-    },
-    // todo:重组excel数据
-    toExcel() {
-      for (let value of this.excel.entries()) {
-        // let centerPoint = value[0]
-        let centerInfo = value[1]
-        centerInfo.push([123, 456])
-        console.log('data', centerInfo)
-        console.log('size', centerInfo.length)
-        console.log('size', centerInfo[1])
-        // 关键字地址个数
-        for (let elem of centerInfo.values()) {
-          console.log(elem)
-        }
-      }
+      console.log(this.list)
+      // 下载excel
+      // this.handleDownload()
     },
     // todo:搜索一个地址的多个关键字
-    searchNearby(item) {
-      var data = []
+    searchNearby(item, cb = null) {
       var that = this
-      var _item = item
+      // var center = item.value
+      var point = item.point
+      var data = []
       // 定位地点
-      this.map.centerAndZoom(new this.BMap.Point(_item[1].point.lng, _item[1].point.lat), this.mapZoom)
+      this.map.centerAndZoom(new this.BMap.Point(point.lng, point.lat), this.mapZoom)
       var options = {
+        // todo:异步回调函数
         onSearchComplete: function (results) {
           if (local.getStatus() === 0) {
             var s = []
@@ -61,28 +66,30 @@ export const excel = {
             for (var i = 0; i < results.getCurrentNumPois(); i++) {
               var x = results.getPoi(i)
               // 计算中心点到结果点的距离
-              var distance = that.map.getDistance(new that.BMap.Point(_item[1].point.lng, _item[1].point.lat), new that.BMap.Point(x.point.lng, x.point.lat))
+              var distance = that.map.getDistance(new that.BMap.Point(point.lng, point.lat), new that.BMap.Point(x.point.lng, x.point.lat))
               distance = distance.toFixed(2) // 保留2位小数
-              var item = {
+              var tmp = {
                 value: x.address + x.title,
                 point: x.point,
                 distance: distance,
                 keyword: results.keyword
               }
-              s.push(item)
+              s.push(tmp)
             }
             let m = {
               key: results.keyword,
               value: s
             }
+            // todo:异步回调
             data.push(m)
+            cb(m)
           }
         }
       }
       var local = new this.BMap.LocalSearch(this.map, options)
       for (let kw of this.keyword.values()) {
         // 对每一个地址都要搜多个关键字。第一个参数：关键字 第二个参数：搜索的中心点 第三个参数：半径 第四各参数：自定义检索数据
-        local.searchNearby(kw, new this.BMap.Point(item[1].point.lng, item[1].point.lat), this.radius * 1000)
+        local.searchNearby(kw, new this.BMap.Point(point.lng, point.lat), this.radius * 1000)
       }
       return data
     },
